6430869ac0ae5b21ed10397fb834c5f1e8160eb0
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2023-04-20 13:20:26 -0700

    Added code to check 1 pattern

--- src/main/java/com/mangat/tictactoe/tictactoe/TicTacToe.java
+++ src/main/java/com/mangat/tictactoe/tictactoe/TicTacToe.java
@@ -10,13 +10,13 @@ public class TicTacToe {
    * 2D int array that contains the representation of the tic-tac-toe.
    * 0 being O and 1 being X.
    */
-  private int[][] playArea;
+  private Integer[][] playArea;
 
   /**
    * Constructs the TicTacToe Object
    */
   public TicTacToe(){
-    playArea = new int[3][3];
+    playArea = new Integer[3][3];
 
   }
 
@@ -25,7 +25,7 @@ public class TicTacToe {
    * Gets the 2D array.
    * @return int[][] which contains the current game.
    */
-  public int[][] getPlayArea() {
+  public Integer[][] getPlayArea() {
     return playArea;
   }
 
@@ -34,7 +34,7 @@ public class TicTacToe {
    * Sets the PlayArea to certain state.
    * @param playArea the new state of the object
    */
-  public void setPlayArea(int[][] playArea) {
+  public void setPlayArea(Integer[][] playArea) {
     this.playArea = playArea;
   }
 
@@ -49,6 +49,68 @@ public class TicTacToe {
     playArea[x][y] = value;
   }
 
+  public String checkState(){
+    /*
+    While to go through each point the on array
+    count the o or 1s on that pattern
+    if got to three then get out of the loop and return the one with three
+     */
+    boolean winningPatternFound = false;
+    String outputOfCheckMethod = "none";
+    int whichPattern = 0;
+
+    while(!winningPatternFound && whichPattern < 8){
+      switch(whichPattern){
+        case 0:
+          outputOfCheckMethod = checkPattern0();
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 6:
+          break;
+        default:
+      }
+      if(!outputOfCheckMethod.equals("none")){
+        winningPatternFound = true;
+      }
+      whichPattern++;
+    }
+
+
+    return outputOfCheckMethod;
+  }
+
+  private String checkPattern0() {
+    String output = "none";
+    int xCount = 0;
+    int oCount = 0;
+    for(int temp : playArea[0]){
+      switch(temp){
+        case 0:
+          oCount++;
+          break;
+        case 1:
+          xCount++;
+          break;
+        default:
+          return output;
+      }
+    }
+    if(xCount == 3 || oCount == 3){
+      output = xCount == 3 ? "X" : "O";
+    }
+
+    return output;
+  }
+
 
   /***
    * Gives the String representation of the 2D Array.
@@ -56,7 +118,7 @@ public class TicTacToe {
    */
   public String toString(){
     StringBuilder output = new StringBuilder();
-    for (int[] ints : playArea) {
+    for (Integer[] ints : playArea) {
       for (int j = 0; j < playArea[0].length; j++) {
         output.append(ints[j] == 0 ? "O" : "X");
       }
